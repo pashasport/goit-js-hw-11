24170a52ba13a23e558a72320f40b5fb9522189a
pashasport <123305807+pashasport@users.noreply.github.com>
2023-07-18 17:28:44 +0300

    check-js-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-BASE_URL = 'https://pixabay.com/api/';
-API_KEY = '38290525-911726ad1b6a7f076609ffcd1';
+const URL = 'https://pixabay.com/api/';
+const KEY = '37440122-e5d5a2493910548fa520b3add';
 
 export async function fetchPhoto(q, page, perPage) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
   const response = await axios.get(url);
   return response.data;
 }
